9f786252b9af2bc597cea540905956863295b40a
Andre Oliveira Beukers <andre.olibeu@gmail.com>
2016-09-22 10:22:21 -0700

    r2z transform

--- analysis_ISFC_video.py
+++ analysis_ISFC_video.py
@@ -1,6 +1,10 @@
 import os
 from os.path import join as opj
+import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
+import scipy as sc
+
 
 # # PATHS
 if "andrebeukers" in os.getcwd():
@@ -10,35 +14,65 @@ elif "srm254" in os.getcwd():
 elif "tmp" in os.getcwd():
     bids_dir = "/Users/andrebeukers/Documents/fMRI/Forrest/BIDSforrest"
 
-# inputs
-sub1 = 3
-sub2 = 4
-run = 3
-node1 = "rPCC"
-node2 = "rPCC"
+sub=1; run=3; node='rPCC'
 
-# paths
-deriv_dir1 = opj(bids_dir, "deriv", "sub_%.2i" % sub1, "ses_video")
-node1_fpath = opj(deriv_dir1, "nodes", "sub_%.2i-run_%.2i-%s_ts.1D" % (sub1, run, node1))
 
-deriv_dir2 = opj(bids_dir, "deriv", "sub_%.2i" % sub2, "ses_video")
-node2_fpath = opj(deriv_dir2, "nodes", "sub_%.2i-run_%.2i-%s_ts.1D" % (sub2, run, node2))
+def get_node_ts(sub,run,node):
+    global bids_dir
+    deriv_dir = opj(bids_dir, "deriv", "sub_%.2i" % sub, "ses_video")
+    node_fpath = opj(deriv_dir, "nodes", "sub_%.2i-run_%.2i-%s_ts.1D" % (sub, run, node))
+    print node_fpath
+    node_ts = pd.read_csv(node_fpath, delimiter=' ', header=None)
+    z_node_ts = sc.stats.zscore(node_ts.iloc[:,0].values)
+    return z_node_ts
 
-# load from .1D to np array
-node1_ts = np.genfromtxt(node1_fpath, delimiter=' ')
-node2_ts = np.genfromtxt(node2_fpath, delimiter=' ')
+ts1 = get_node_ts(1,run,node)
+ts2 = get_node_ts(2,run,node)
 
-# sliding window (in TRs) correlation
-window_size = 60
-corr = []
-for t in range(len(node1_ts) - window_size):
-    node1_ts_seg = node1_ts[ t : t+window_size ]
-    node2_ts_seg = node2_ts[ t : t+window_size ]
-    corr_seg = np.correlate(node1_ts_seg, node2_ts_seg)
-    corr.extend(corr_seg)
+
+def slidingISFC(sub1,sub2,run,node,window_size):
+    node_ts1 = get_node_ts(sub1,run,node)
+    node_ts2 = get_node_ts(sub2,run,node)
+    assert len(node_ts1) == len(node_ts2)
+    
+    corr = []
+    # sliding window (in TRs) correlation
+    for t in range(len(node_ts1) - window_size):
+        # get segment
+        node_ts1_seg = node_ts1[ t : t+window_size ]
+        node_ts2_seg = node_ts2[ t : t+window_size ]
+        # correlate segments
+        # assert (np.correlate(node_ts1_seg, node_ts2_seg) 
+             # == np.correlate(node_ts2_seg, node_ts1_seg))
+        # collect correlation 
+        
+        corr.extend(np.correlate(node_ts1_seg, node_ts2_seg))
+
+    # z = np.log((1 + r) / (1 - r)) * (np.sqrt(n - 3) / 2)
+    # r-to-z transform
+    n = len(corr)
+    z_corr = np.log((1 + corr) / (1 - corr)) * (np.sqrt(n - 3) / 2)
+    return z_corr
+
+
+
+ts1 = slidingISFC(1,2,3,'rIPL',window_size)
+plt.plot(ts1)
+
+ts2 = slidingISFC(1,4,3,'rIPL',window_size)
+plt.plot(ts2)
+
+plt.show()
+
+
+# ts3=slidingISFC(2,4,3,node,window_size)
+# plt.plot(ts3)
+
+
+
 
 # save results
-result_fname = "ISFC-%ss%.2i-%ss%.2i-r%.2i" % (node1, sub1, node2, sub2, run)
-result_fpath = opj(bids_dir, "deriv", "analysis_ISFC", result_fname)
-np.savetxt(result_fpath, corr)
+# result_fname = "ISFC-%ss%.2i-%ss%.2i-r%.2i" % (node1, sub1, node2, sub2, run)
+# result_fpath = opj(bids_dir, "deriv", "analysis_ISFC", result_fname)
+# np.savetxt(result_fpath, corr)
     
